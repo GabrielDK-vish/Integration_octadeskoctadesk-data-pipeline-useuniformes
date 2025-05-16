c224503b6846e0d9879ea047084b46f04a577374
Gabriel <negociogabriel2k25@outlook.com>
2025-05-16 15:30:13 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 from pathlib import Path
+from google.oauth2 import service_account
+from google.cloud import bigquery
 import os
+import pytz
 
 dotenv_path = Path(__file__).parent / ".env"
 load_dotenv(dotenv_path=dotenv_path)
@@ -16,3 +19,22 @@ missing = [n for n,v in [
 ] if not v]
 if missing:
     raise RuntimeError(f"Faltando variáveis no .env: {missing}")
+
+CONFIG_PATH = Path(__file__).parent / "config.json"
+CREDS       = service_account.Credentials.from_service_account_file(CONFIG_PATH)
+PROJECT     = CREDS.project_id
+
+if PROJECT is None:
+    raise RuntimeError(f"ID do projeto não encontrado nas credenciais.")
+
+BQ = bigquery.Client(credentials=CREDS, project=PROJECT)
+TIMEZONE = pytz.timezone("America/Sao_Paulo")
+SRC_TABLE_SAC_OCTADESK = f"{PROJECT}.DataLake_2025.Octadesk"
+SRC_TABLE_TICKETS_ABERTOS = f"{PROJECT}.DataWareHouse_2025.Sac_TicketsAbertos"
+
+OCTA_HEADERS = {
+    "Content-Type":      "application/json",
+    "Accept":            "application/json",
+    "x-api-key":         OCTA_API_KEY,
+    "octa-agent-email":  OCTA_AGENT_EMAIL,
+}
